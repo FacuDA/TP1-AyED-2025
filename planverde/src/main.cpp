f5ca1219ae93088b03e438c6720cab88b39c58e0
FacuDA <118486535+FacuDA@users.noreply.github.com>
2025-06-10 22:23:40 -0300

    Update main.cpp
    cambie la estructura del codigo para poder cargar y mostrar los datos sin problemas

--- planverde/src/main.cpp
+++ planverde/src/main.cpp
@@ -1,44 +1,49 @@
 #include <iostream>
 #include <string>
-#include "include/main.h"   // Incluye el main header file.
-#include "include/id.h"      // Incluye el id header file.
+#include "main.h" 
 
 using namespace std;
 
 int interaction = 0;
 
 int main() {
+    Huerta datosHuerta;
+
     cout << "\nBienvenido al proyecto de Huertas Urbanas de Plan Verde!" << endl;
     cout << "Por favor, ingrese el número de la acción que desea realizar:" << endl;
     cout << "1. Cargar y procesar datos de una huerta" << endl;
     cout << "2. Mostrar resultados" << endl;
     cout << "3. Salir" << endl;
 
-    while (1) {
-        huerta datosHuerta;
+    while (true) {
         cin >> interaction;
         switch (interaction) {
             case 1:
                 datosHuerta.id = generarID();
-                cout << "Usted ha elegido cargar y procesar datos de una huerta. Se le ha asignado el número '" << datosHuerta.id << "' como identificador de huerta." << endl;
-                cargarDatos();
-                interaction = 0;
+                cout << "Usted ha elegido cargar y procesar datos de una huerta." << endl;
+                cout << "Se le ha asignado el número '" << datosHuerta.id << "' como identificador de huerta." << endl;
+                datosHuerta = cargarDatos();
                 break;
+
             case 2:
                 cout << "Usted ha elegido mostrar los resultados de la huerta." << endl;
-                mostrarDatos();
-                interaction = 0;
+                mostrarDatos(datosHuerta);
                 break;
+
             case 3:
                 cout << "Usted ha elegido salir del programa." << endl;
-                interaction = 0;
-                abort();
-                break;
+                return 0;
+
             default:
                 cout << "Opción no válida. Por favor, intente nuevamente." << endl;
-                interaction = 0;
                 break;
         }
+
+        cout << "\n¿Qué desea hacer a continuación?" << endl;
+        cout << "1. Cargar y procesar datos de una huerta" << endl;
+        cout << "2. Mostrar resultados" << endl;
+        cout << "3. Salir" << endl;
     }
+
     return 0;
 }
